e7b4f54e47f211bce076c9f69f103f2455594f7f
brgndy <brgndy96@gmail.com>
2023-03-11 00:05:33 +0900

    msg

--- 20230310blog/components/HomeFeed/HomeFeed.tsx
+++ 20230310blog/components/HomeFeed/HomeFeed.tsx
@@ -1,5 +1,20 @@
 import classes from "./HomeFeed.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function HomeFeed() {
-  return <div className={classes.homeFeed_container}></div>;
+  return (
+    <div className={classes.homeFeed_container}>
+      <div className={classes.item}>
+        <Link href={"/exhibiton"}>
+          <Image
+            src={"/image/exhibition.png"}
+            alt={"itemImage"}
+            width={500}
+            height={500}
+          />
+        </Link>
+      </div>
+    </div>
+  );
 }
