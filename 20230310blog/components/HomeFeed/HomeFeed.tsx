b80ca312e70c4ea47ba373e8827970ae817be9e3
brgndy <brgndy96@gmail.com>
2023-03-12 22:42:01 +0900

    msg

--- 20230310blog/components/HomeFeed/HomeFeed.tsx
+++ 20230310blog/components/HomeFeed/HomeFeed.tsx
@@ -5,15 +5,66 @@ import Link from "next/link";
 export default function HomeFeed() {
   return (
     <div className={classes.homeFeed_container}>
-      <div className={classes.item}>
+      <div className={classes.item_container}>
         <Link href={"/exhibiton"}>
           <Image
+            className={classes.category_img}
             src={"/image/exhibition.png"}
-            alt={"itemImage"}
-            width={500}
-            height={500}
+            alt={"exhibiton"}
+            width={300}
+            height={300}
           />
         </Link>
+      </div>
+      <div className={classes.item_container}>
+        <Link href={"/movie"}>
+          <Image
+            className={classes.category_img}
+            src={"/image/movie.jpeg"}
+            alt={"movie"}
+            width={300}
+            height={300}
+          />
+        </Link>
+      </div>
+      <div className={classes.item_container}>
+        <div className={classes.category_item}>
+          <Link href={"/exhibiton"} className={classes.link}>
+            <Image
+              className={classes.category_img}
+              src={"/image/book.jpeg"}
+              alt={"book"}
+              width={300}
+              height={300}
+            />
+          </Link>
+        </div>
+      </div>
+      <div className={classes.item_container}>
+        <div className={classes.category_item}>
+          <Link href={"/exhibiton"}>
+            <Image
+              className={classes.category_img}
+              src={"/image/exhibition.png"}
+              alt={"itemImage"}
+              width={300}
+              height={300}
+            />
+          </Link>
+        </div>
+      </div>
+      <div className={classes.item_container}>
+        <div className={classes.category_item}>
+          <Link href={"/exhibiton"}>
+            <Image
+              className={classes.category_img}
+              src={"/image/exhibition.png"}
+              alt={"itemImage"}
+              width={300}
+              height={300}
+            />
+          </Link>
+        </div>
       </div>
     </div>
   );
