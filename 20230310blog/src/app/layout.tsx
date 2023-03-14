fbfd3cdfb713aa70bb6ea3d41e13da4d1eb96226
brgndy <brgndy96@gmail.com>
2023-03-14 17:32:02 +0900

    msg

--- 20230310blog/src/app/layout.tsx
+++ 20230310blog/src/app/layout.tsx
@@ -11,7 +11,9 @@ export default function RootLayout({
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
+
       <head />
+
       <body>
         <div id="modal_portal"></div>
         {children}
