352a2c11f094b454b69af7a9241e8ca452eab9de
brgndy <brgndy96@gmail.com>
2023-03-14 17:32:06 +0900

    msg

--- 20230310blog/components/Modal/Modal.tsx
+++ 20230310blog/components/Modal/Modal.tsx
@@ -1,11 +1,30 @@
 "use client";
 
 import classes from "./Modal.module.css";
+import ModalList from "./ModalList";
 
 type Props = {
   onClose: () => void;
   modal: boolean;
 };
+
+const DUMMY_Category = [
+  {
+    id: 1,
+    category: "음악",
+    href: "music",
+  },
+  {
+    id: 2,
+    category: "영화",
+    href: "/movie",
+  },
+  { id: 3, category: "책", href: "book" },
+
+  { id: 4, category: "전시", href: "exihibiton" },
+
+  { id: 5, category: "기타", href: "etc" },
+];
 
 export default function Modal({ modal, onClose }: Props) {
   console.log("Modal의 현재 modal 상태는 : ", modal);
@@ -13,7 +32,17 @@ export default function Modal({ modal, onClose }: Props) {
     <>
       <nav className={`${classes.modal_container} ${modal && classes.show}`}>
         <div className={classes.modal_list_container}>
-          <ul></ul>
+          <ul className={classes.modal_ul}>
+            {DUMMY_Category.map((item) => {
+              return (
+                <ModalList
+                  key={item.id}
+                  category={item.category}
+                  href={item.href}
+                />
+              );
+            })}
+          </ul>
         </div>
       </nav>
     </>
--- 20230310blog/components/Modal/ModalList.tsx
+++ 20230310blog/components/Modal/ModalList.tsx
@@ -1,5 +1,22 @@
 import React from "react";
+import classes from "./ModalList.module.css";
+import Link from "next/link";
 
-export default function ModalList() {
-  return <div>ModalList</div>;
+type Props = {
+  category: string;
+  href: string;
+};
+
+export default function ModalList({ category, href }: Props) {
+  return (
+    <>
+      <div className={classes.cateogry_card}>
+        <Link href={`${href}`} className={classes.category_link}>
+          <li className={classes.category}>
+            <p>{category}</p>
+          </li>
+        </Link>
+      </div>
+    </>
+  );
 }
