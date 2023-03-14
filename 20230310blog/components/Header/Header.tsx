8263d7155907a7c38832dfe347514a186e13762c
brgndy <brgndy96@gmail.com>
2023-03-14 09:53:23 +0900

    msg

--- 20230310blog/components/Header/Header.tsx
+++ 20230310blog/components/Header/Header.tsx
@@ -1,13 +1,22 @@
 import Link from "next/link";
 import classes from "./Header.module.css";
+import Image from "next/image";
+import thumbnailImg from "../../public/image/thumbnail.svg";
 
 export default function Header() {
   return (
     <>
       <header className={classes.header_container}>
-        <Link href={"/"}>
-          <h1 className={classes.header_title}>BRGNDY</h1>
-        </Link>
+        <div className={classes.header_card}>
+          <Link href={"/"}>
+            <Image
+              src={thumbnailImg}
+              alt="thumbnail"
+              width={450}
+              height={130}
+            />
+          </Link>
+        </div>
       </header>
     </>
   );
