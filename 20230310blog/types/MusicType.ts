debd1fa482f71cf1ed61358c203df7686d3b919e
brgndy <brgndy96@gmail.com>
2023-03-15 15:50:23 +0900

    msg

---
+++ 20230310blog/types/MusicType.ts
@@ -0,0 +1,11 @@
+export type Music = {
+  id: number;
+  category: string;
+  title: string;
+  artist: string;
+  released: string;
+  rate: number;
+  summary: string;
+  review: string;
+  image: string;
+};
