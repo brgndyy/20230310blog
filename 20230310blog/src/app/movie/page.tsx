f2cd3f4da7baae8564361e89a6f143f1b14cfd07
brgndy <brgndy96@gmail.com>
2023-03-17 00:14:57 +0900

    msg

--- 20230310blog/src/app/movie/page.tsx
+++ 20230310blog/src/app/movie/page.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Card from "components/HomeCard/Card";
 import Header from "components/Header/Header";
+import { dummyMovieData } from "DUMMY_DATA/movie/dummy_movie.data";
+import PostList from "components/Music/PostList";
 
 export default function Page() {
   return (
     <>
       <Card>
         <Header />
+        <PostList contentList={dummyMovieData} />
       </Card>
     </>
   );
