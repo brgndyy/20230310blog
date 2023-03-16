96805fc5deb673ea38ad1a7296ecb77bdf20cff8
brgndy <brgndy96@gmail.com>
2023-03-16 23:29:36 +0900

    msg

--- 20230310blog/src/app/music/page.tsx
+++ 20230310blog/src/app/music/page.tsx
@@ -10,7 +10,7 @@ export default function Page() {
     <>
       <Card>
         <Header />
-        <PostList dummyMusicData={dummyMusicData} />
+        <PostList contentList={dummyMusicData} />
       </Card>
     </>
   );
---
+++ 20230310blog/types/ContentType.ts
@@ -0,0 +1,6 @@
+export type ContentType = {
+  id: number;
+  title: string;
+  imageUrl: string;
+  [key: string]: any;
+};
