d601ae078a7856f126d8edfad44e1bd1910e0bb2
brgndy <brgndy96@gmail.com>
2023-03-23 10:35:08 +0900

    msg

---
+++ 20230310blog/components/KeyBoard/KeyBoard.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import classes from "./KeyBoard.module.css";
+
+export default function KeyBoard() {
+  const [inputString, setInputString] = useState("");
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleKeyDown = (e: any) => {
+      setInputString((prevState) => prevState + e.key);
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      // Cleanup - remove event listener when component unmounts
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (inputString.includes("brgndyadmin")) {
+      router.push("/login");
+    }
+  }, [inputString, router]);
+
+  return <div className={classes.keyboard_div}>KeyBoard</div>;
+}
--- 20230310blog/src/app/page.tsx
+++ 20230310blog/src/app/page.tsx
@@ -1,6 +1,7 @@
 import Header from "components/Header/Header";
 import HomeFeed from "components/HomeFeed/HomeFeed";
 import Card from "components/HomeCard/Card";
+import KeyBoard from "components/KeyBoard/KeyBoard";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Card>
         <Header />
         <HomeFeed />
+        <KeyBoard />
       </Card>
     </>
   );
